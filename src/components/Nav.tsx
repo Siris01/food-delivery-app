e22fdc2af828acb1d99f1b3c013b559715d4c9e4
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-04-07 14:44:09 +0530

    chore: fmt

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,61 +3,86 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 const links = [
-    { title: 'Home', href: '/' },
-    { title: 'Order', href: '/order' },
-    { title: 'Cart', href: '/cart' },
-    { title: 'Account', href: '/account' },
-]
+	{ title: 'Home', href: '/' },
+	{ title: 'Order', href: '/order' },
+	{ title: 'Cart', href: '/cart' },
+	{ title: 'Account', href: '/account' }
+];
 
 export default function Nav() {
-    const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-    return (
-        <nav className='sticky top-0 z-50 before:border-b-2 before:-z-1 before:content-[""] before:absolute before:w-full before:h-full before:backdrop-blur-lg before:backdrop-brightness-25 before:bg-black/30 before:border-black'>
-            <div className="relative text-white flex items-center justify-between px-4 py-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div className="flex items-center flex-1">
-                    <Link href='/' className="flex items-center justify-center">
-                        <Logo size={48} />
-                        <span className='ml-4 font-bold text-lg'>FDA</span>
-                    </Link>
-                    <div className="hidden ml-10 space-x-8 lg:block">
-                        {links.map((item) => (
-                            <Link key={item.href} href={item.href} className="text-lg font-medium hover:text-primary">
-                                {item.title}
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-                <div className="hidden lg:flex items-center justify-end lg:justify-center">
-                    <Link href="/login" className="font-bold text-lg px-4 p-2 m-2 bg-primary hover:bg-primary/70 text-black rounded-md">
-                        Log In
-                    </Link>
-                </div>
-                <div className="-mr-2 -my-2 lg:hidden">
-                    <button onClick={() => setOpen(o => !o)} type='button' className="inline-flex items-center justify-center p-2 bg-primary text-black rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-            <div className={`lg:hidden relative bg-slate/30 backdrop-blur-lg backdrop-brightness-25 ${open ? 'translate-y-0' : 'h-0 -translate-y-[400px]'}`}>
-                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    {links.map((item) => (
-                        <Link key={item.href} href={item.href} className="block px-3 py-2 text-base font-medium hover:text-primary rounded-md">
-                            {item.title}
-                        </Link>
-                    ))}
-                </div>
-                <div className="px-5 py-4 space-y-4">
-                    <div>
-                        <Link href="/login" className="block w-full px-5 py-3 text-base font-bold text-center bg-primary hover:bg-primary/70 text-black border border-transparent rounded-md shadow-sm">
-                            Log In
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+	return (
+		<nav className='sticky top-0 z-50 before:border-b-2 before:-z-1 before:content-[""] before:absolute before:w-full before:h-full before:backdrop-blur-lg before:backdrop-brightness-25 before:bg-black/30 before:border-black'>
+			<div className='relative text-white flex items-center justify-between px-4 py-2 mx-auto max-w-7xl sm:px-6 lg:px-8'>
+				<div className='flex items-center flex-1'>
+					<Link href='/' className='flex items-center justify-center'>
+						<Logo size={48} />
+						<span className='ml-4 font-bold text-lg'>FDA</span>
+					</Link>
+					<div className='hidden ml-10 space-x-8 lg:block'>
+						{links.map((item) => (
+							<Link key={item.href} href={item.href} className='text-lg font-medium hover:text-primary'>
+								{item.title}
+							</Link>
+						))}
+					</div>
+				</div>
+				<div className='hidden lg:flex items-center justify-end lg:justify-center'>
+					<Link
+						href='/login'
+						className='font-bold text-lg px-4 p-2 m-2 bg-primary hover:bg-primary/70 text-black rounded-md'
+					>
+						Log In
+					</Link>
+				</div>
+				<div className='-mr-2 -my-2 lg:hidden'>
+					<button
+						onClick={() => setOpen((o) => !o)}
+						type='button'
+						className='inline-flex items-center justify-center p-2 bg-primary text-black rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary'
+					>
+						<span className='sr-only'>Open main menu</span>
+						<svg
+							className='w-6 h-6'
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke='currentColor'
+							aria-hidden='true'
+						>
+							<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16' />
+						</svg>
+					</button>
+				</div>
+			</div>
+			<div
+				className={`lg:hidden relative bg-slate/30 backdrop-blur-lg backdrop-brightness-25 ${
+					open ? 'translate-y-0' : 'h-0 -translate-y-[400px]'
+				}`}
+			>
+				<div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
+					{links.map((item) => (
+						<Link
+							key={item.href}
+							href={item.href}
+							className='block px-3 py-2 text-base font-medium hover:text-primary rounded-md'
+						>
+							{item.title}
+						</Link>
+					))}
+				</div>
+				<div className='px-5 py-4 space-y-4'>
+					<div>
+						<Link
+							href='/login'
+							className='block w-full px-5 py-3 text-base font-bold text-center bg-primary hover:bg-primary/70 text-black border border-transparent rounded-md shadow-sm'
+						>
+							Log In
+						</Link>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,18 +6,24 @@ import Link from 'next/link';
 const Home: NextPage = () => {
 	return (
 		<div className='flex flex-col p-4 m-12 justify-center items-center'>
-			<span className='font-extrabold p-2 m-2 text-6xl md:text-7xl lg:text-8xl text-transparent bg-clip-text bg-gradient-to-r from-[#f12711] to-[#f5af19]'>What are you craving for today?</span>
+			<span className='font-extrabold p-2 m-2 text-6xl md:text-7xl lg:text-8xl text-transparent bg-clip-text bg-gradient-to-r from-[#f12711] to-[#f5af19]'>
+				What are you craving for today?
+			</span>
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
 				{cuisines.map((cuisine) => {
 					return (
-						<Link href={`/order?cuisine=${cuisine.title.toLowerCase()}`} key={cuisine.title.toLowerCase()} className='flex flex-row justify-center items-center p-4 m-4 bg-black/70 rounded-md cursor-pointer border-2 border-black hover:border-primary'>
+						<Link
+							href={`/order?cuisine=${cuisine.title.toLowerCase()}`}
+							key={cuisine.title.toLowerCase()}
+							className='flex flex-row justify-center items-center p-4 m-4 bg-black/70 rounded-md cursor-pointer border-2 border-black hover:border-primary'
+						>
 							<Image src={`/icons/${cuisine.icon}.svg`} alt={cuisine.title} width={256} height={256} />
 							<div className='flex flex-col justify-between ml-4'>
 								<span className='font-bold text-lg'>{cuisine.title}</span>
 								<span className='font-medium text-base'>{cuisine.description}</span>
 							</div>
 						</Link>
-					)
+					);
 				})}
 			</div>
 		</div>
