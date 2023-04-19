0f76c9d82d9a3aac5a4d55a46954bae43945011e
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-04-19 13:32:03 +0530

    chore: fix component names

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,14 +11,13 @@ interface InputProps {
 	fullyRounded?: boolean;
 }
 
-const Logo = (props: InputProps) => {
+const Input = (props: InputProps) => {
 	return (
 		<div className='flex flex-row'>
 			{props.leftIcon && (
 				<div
-					className={`flex items-center justify-center w-10 h-10 border-2 border-r-0 border-primary/40 peer-focus:border-primary text-white ${
-						props.fullyRounded ? 'rounded-l-full' : 'rounded-l-md'
-					} ${props.iconBG ? 'bg-primary' : 'bg-zinc-900'}`}
+					className={`flex items-center justify-center w-10 h-10 border-2 border-r-0 border-primary/40 peer-focus:border-primary text-white ${props.fullyRounded ? 'rounded-l-full' : 'rounded-l-md'
+						} ${props.iconBG ? 'bg-primary' : 'bg-zinc-900'}`}
 				>
 					{props.leftIcon}
 				</div>
@@ -48,15 +47,13 @@ const Logo = (props: InputProps) => {
 				}}
 				placeholder={props.placeholder}
 				type={props.type}
-				className={`px-4 py-2 h-10 text-white/70 bg-zinc-900 focus:outline-none peer border-2 border-primary/40 focus:border-primary ${
-					props.fullyRounded ? 'rounded-full' : 'rounded-md'
-				} ${props.leftIcon && '!rounded-l-none border-l-0'} ${props.rightIcon && '!rounded-r-none border-r-0'}`}
+				className={`px-4 py-2 h-10 text-white/70 bg-zinc-900 focus:outline-none peer border-2 border-primary/40 focus:border-primary ${props.fullyRounded ? 'rounded-full' : 'rounded-md'
+					} ${props.leftIcon && '!rounded-l-none border-l-0'} ${props.rightIcon && '!rounded-r-none border-r-0'}`}
 			/>
 			{props.rightIcon && (
 				<div
-					className={`flex items-center justify-center w-10 h-10 border-2 border-l-0 border-primary/40 peer-focus:border-primary text-white ${
-						props.fullyRounded ? 'rounded-r-full' : 'rounded-r-md'
-					} ${props.iconBG ? 'bg-primary' : 'bg-zinc-900'}`}
+					className={`flex items-center justify-center w-10 h-10 border-2 border-l-0 border-primary/40 peer-focus:border-primary text-white ${props.fullyRounded ? 'rounded-r-full' : 'rounded-r-md'
+						} ${props.iconBG ? 'bg-primary' : 'bg-zinc-900'}`}
 				>
 					{props.rightIcon}
 				</div>
@@ -65,7 +62,7 @@ const Logo = (props: InputProps) => {
 	);
 };
 
-export default Logo;
+export default Input;
 
 const getNextInput = (input: HTMLInputElement, dir = 'down'): HTMLInputElement | null => {
 	const inputs = Array.from(document.querySelectorAll('input'));
